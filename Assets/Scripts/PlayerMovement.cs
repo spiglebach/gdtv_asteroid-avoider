1d0784b792d7fb78438f3b02515336a87d4b5156
Szukács Marcell <spiglebach@gmail.com>
2021-09-22 20:10:23 +0200

    add physics based spaceship movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,13 @@ using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class PlayerMovement : MonoBehaviour {
+    [SerializeField] private float forceMagnitude = 500;
+    [SerializeField] private float maxVelocity = 6;
+
+    private Vector3 movementDirection;
+    
     private Camera mainCamera;
+    private Rigidbody rigidbody;
 
     private void OnEnable() {
         EnhancedTouchSupport.Enable();
@@ -15,14 +21,26 @@ public class PlayerMovement : MonoBehaviour {
 
     void Start() {
         mainCamera = Camera.main;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update() {
         var activeTouches = Touch.activeTouches;
-        if (activeTouches.Count <= 0) return;
+        if (activeTouches.Count <= 0) {
+            movementDirection = Vector3.zero;
+            return;
+        }
         var touch = activeTouches[0];
         var touchWorldPosition = mainCamera.ScreenToWorldPoint(touch.screenPosition);
         touchWorldPosition.y = 0;
-        transform.SetPositionAndRotation(touchWorldPosition, Quaternion.identity);
+
+        movementDirection = transform.position - touchWorldPosition;
+        movementDirection.Normalize();
+    }
+
+    private void FixedUpdate() {
+        if (movementDirection == Vector3.zero) return;
+        rigidbody.AddForce(movementDirection * forceMagnitude);
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVelocity);
     }
 }
